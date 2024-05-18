b66fa712b75a6eb574278e545efe439f2df8eba3
SAMUEL BEDOYA FLOREZ <samuelbedoya309347@correo.itm.edu.co>
2024-05-18 15:27:59 -0500

    DatabaseContext class created with db conextion and Tasks DbSet

---
+++ TaskMangment/TaskMangment/DAL/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.Metrics;
+using TaskMangment.DAL.Entities;
+
+namespace TaskMangment.DAL
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)//conexion a la BD
+        {
+
+        }
+
+        public DbSet<Tasks> Tasks { get; set; }
+    }
+}
